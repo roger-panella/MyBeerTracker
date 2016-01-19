9d2f5d510c807d314efc86c836e4526990b83d49
Roger Panella <rogerpanella@gmail.com>
2016-01-19 17:35:33 -0600

    basic routes and controllers

--- app_server/controllers/main.js
+++ app_server/controllers/main.js
@@ -1,5 +1,15 @@
-
-
 module.exports.index = function(req, res){
   res.render('index', { title: 'Express'});
+};
+
+module.exports.cellar = function(req, res){
+  res.render('cellar', { title: 'Your Cellar'});
+};
+
+module.exports.publicCellar = function(req, res){
+  res.render('public_cellar', { title: 'Public Cellar'});
+};
+
+module.exports.about = function(req, res){
+  res.render('about', { title: 'About My Beer Tracker'});
 };
--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -6,6 +6,8 @@ var ctrlMain = require('../controllers/main');
 
 /* GET home page. */
 router.get('/', ctrlMain.index);
-
+router.get('/cellar', ctrlMain.cellar);
+router.get('/public_cellar', ctrlMain.publicCellar);
+router.get('/about', ctrlMain.about);
 
 module.exports = router;
