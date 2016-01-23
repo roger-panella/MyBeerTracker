5efea30d03fdd717f82c2b0e94db023f3c03126d
Roger Panella <rogerpanella@gmail.com>
2016-01-22 23:00:40 -0600

    added divs for beer search results

--- public/javascripts/javascript.js
+++ public/javascripts/javascript.js
@@ -36,10 +36,31 @@ function displayBeers(beerResults){
   beerList.id = "beer-list";
   searchResultsDiv.appendChild(beerList);
   for (var i = 0; i < beerResults.length;i++) {
+    var singleBeerDiv = document.createElement("div");
+    singleBeerDiv.id = "single-beer-result";
     var singleBeerListElement = document.createElement("li");
     singleBeerListElement.innerHTML = '<a href="/add?q=' +beerResults[i] +'">'+beerResults[i]+'</a>';
     singleBeerListElement.id = beerResults[i];
+    singleBeerDiv.appendChild(singleBeerListElement);
     var allBeersList = document.getElementById("beer-list");
-    allBeersList.appendChild(singleBeerListElement);
+    allBeersList.appendChild(singleBeerDiv);
   }
 };
+
+
+
+
+
+// function displayBeers(beerResults){
+//   var searchResultsDiv = document.getElementById('beer-results');
+//   var beerList = document.createElement("ul");
+//   beerList.id = "beer-list";
+//   searchResultsDiv.appendChild(beerList);
+//   for (var i = 0; i < beerResults.length;i++) {
+//     var singleBeerListElement = document.createElement("li");
+//     singleBeerListElement.innerHTML = '<a href="/add?q=' +beerResults[i] +'">'+beerResults[i]+'</a>';
+//     singleBeerListElement.id = beerResults[i];
+//     var allBeersList = document.getElementById("beer-list");
+//     allBeersList.appendChild(singleBeerListElement);
+//   }
+// };
