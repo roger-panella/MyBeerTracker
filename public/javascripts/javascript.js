062f0383e497a6f111025639f036058d10d2d70c
Roger Panella <rogerpanella@gmail.com>
2016-01-22 23:52:27 -0600

    added objects for beer search

--- public/javascripts/javascript.js
+++ public/javascripts/javascript.js
@@ -21,14 +21,23 @@ function searchUntappd(searchParams){
   })
 };
 
+// Beer search results as objects -- testing!
+
 function getTenBeers(data) {
+
   var beerResults = [];
   for (var i = 0; i < data.response.beers.items.length; i++) {
-    beerResults.push((data.response.beers.items[i].brewery.brewery_name) + ' ' + (data.response.beers.items[i].beer.beer_name));
-    console.log(beerResults);
+    var beerObject = {};
+    beerObject["brewery"] = data.response.beers.items[i].brewery.brewery_name;
+    beerObject["beerName"] = data.response.beers.items[i].beer.beer_name;
+    beerObject["imageUrl"] = data.response.beers.items[i].beer.beer_label;
+    beerResults.push(beerObject);
   };
+  console.log(beerResults);
    displayBeers(beerResults);
 };
+
+// Beer rednering as objects -- testing!
 
 function displayBeers(beerResults){
   var searchResultsDiv = document.getElementById('beer-results');
@@ -39,8 +48,7 @@ function displayBeers(beerResults){
     var singleBeerDiv = document.createElement("div");
     singleBeerDiv.id = "single-beer-result";
     var singleBeerListElement = document.createElement("li");
-    singleBeerListElement.innerHTML = '<a href="/add?q=' +beerResults[i] +'">'+beerResults[i]+'</a>';
-    singleBeerListElement.id = beerResults[i];
+    singleBeerListElement.innerHTML = '<a href="/add?q='+beerResults[i].brewery+beerResults[i].beerName+'">'+beerResults[i].brewery+ ' ' + beerResults[i].beerName+'</a>';
     singleBeerDiv.appendChild(singleBeerListElement);
     var allBeersList = document.getElementById("beer-list");
     allBeersList.appendChild(singleBeerDiv);
@@ -51,16 +59,38 @@ function displayBeers(beerResults){
 
 
 
+
+
+
+
+
+
+
+
+// Working Code with Arrays and not OBjects!!!!
+
+// function getTenBeers(data) {
+//   var beerResults = [];
+//   for (var i = 0; i < data.response.beers.items.length; i++) {
+//     beerResults.push((data.response.beers.items[i].brewery.brewery_name) + ' ' + (data.response.beers.items[i].beer.beer_name));
+//     console.log(beerResults);
+//   };
+//    displayBeers(beerResults);
+// };
+//
 // function displayBeers(beerResults){
 //   var searchResultsDiv = document.getElementById('beer-results');
 //   var beerList = document.createElement("ul");
 //   beerList.id = "beer-list";
 //   searchResultsDiv.appendChild(beerList);
 //   for (var i = 0; i < beerResults.length;i++) {
+//     var singleBeerDiv = document.createElement("div");
+//     singleBeerDiv.id = "single-beer-result";
 //     var singleBeerListElement = document.createElement("li");
 //     singleBeerListElement.innerHTML = '<a href="/add?q=' +beerResults[i] +'">'+beerResults[i]+'</a>';
 //     singleBeerListElement.id = beerResults[i];
+//     singleBeerDiv.appendChild(singleBeerListElement);
 //     var allBeersList = document.getElementById("beer-list");
-//     allBeersList.appendChild(singleBeerListElement);
+//     allBeersList.appendChild(singleBeerDiv);
 //   }
 // };
