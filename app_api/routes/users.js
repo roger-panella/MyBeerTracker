ebc478abf64a861974e253ee6687f8962d764d54
Roger Panella <rogerpanella@gmail.com>
2016-01-25 19:08:05 -0600

    login registration before changing models

--- app_api/routes/users.js
+++ app_api/routes/users.js
@@ -3,7 +3,6 @@ var passport = require('passport');
 var LocalStrategy = require('passport-local').Strategy;
 var User = require('../models/User');
 var router = express.Router();
-
 passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
