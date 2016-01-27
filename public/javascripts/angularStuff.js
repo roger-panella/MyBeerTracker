3061df888348a4f6aa0364c107ee2a929ad9c493
Roger Panella <rogerpanella@gmail.com>
2016-01-27 09:37:59 -0600

    ng app moved to html tag

--- public/javascripts/angularStuff.js
+++ public/javascripts/angularStuff.js
@@ -1,8 +1,5 @@
 var beerTracker = angular.module('beerTracker',[]);
 
-// angular.module('beerTracker',[]).config(function($interpolateProvider){
-//     $interpolateProvider.startSymbol('{[{').endSymbol('}]}');
-// });
 beerTracker.config(function($interpolateProvider){
     $interpolateProvider.startSymbol('//');
     $interpolateProvider.endSymbol('//');
