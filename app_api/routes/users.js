067478c41e587dc0be2455c1c9a55c46370ab1e2
Roger Panella <rogerpanella@gmail.com>
2016-02-17 12:32:11 -0600

    reset password route and view

--- app_api/routes/users.js
+++ app_api/routes/users.js
@@ -121,6 +121,18 @@ router.post('/forgot-password', function(req, res, next){
   });
 });
 
+router.get('/reset/:token', function(req, res){
+  User.findOne({ resetPasswordToken: req.params.token, resetPasswordExpires: { $gt: Date.now() } }, function(err, user){
+    if (!user) {
+      req.flash('error', 'Password reset token is invalid or has expired');
+      return res.redirect('/forgot-password');
+    }
+    res.render('reset', {
+      user: req.user
+    });
+  });
+});
+
 
 
 
