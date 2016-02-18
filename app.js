17141546a0012b67e74b2d836fd16a991a2ac2d3
Roger Panella <rogerpanella@gmail.com>
2016-02-18 00:11:15 -0600

    testing favicon

--- app.js
+++ app.js
@@ -119,7 +119,7 @@ hbs.registerHelper("getStylesForSort", function(beers){
 
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+// app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
